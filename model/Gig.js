7594fa208d0c40b09875983ddf33ebe524cd55e0
GFT <gaaftechsolution@gmail.com>
2024-03-30 14:48:44 +0100

    initial commit

--- model/Gig.js
+++ model/Gig.js
@@ -1,36 +1,5 @@
 const mongoose = require('mongoose');
 
-// Define a sub-schema for feedback
-/* const feedbackSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-    },
-    country: {
-        type: String,
-        required: true,
-    },
-    message: {
-        type: String,
-        required: true,
-    },
-    starRating: {
-        type: Number,
-        required: true,
-        min: 1,
-        max: 5,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-}); */
-
-// Define the main gig schema
 const gigSchema = new mongoose.Schema({ 
     title: {
         type: String,
--- route/feedBack.js
+++ route/feedBack.js
@@ -1,5 +1,3 @@
-// routes/gigRoutes.js
-
 const express = require('express');
 const router = express.Router();
 const Gig = require('../model/Gig');
